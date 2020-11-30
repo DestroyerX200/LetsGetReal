0e3887820e5ce1a95466bce3a384975dde42e4da
i has candys <ichen10@stuy.edu>
2020-11-29 21:50:07 -0500

    modified toString method in RationalNumber.java to account denominator of 1, or numerator of 0

--- Tester2.java
+++ Tester2.java
@@ -16,5 +16,18 @@ public class Tester2 {
 		System.out.print(a.compareTo(c)); System.out.println(" ---> 0");
 		System.out.print(a.compareTo(epsilon)); System.out.println(" ---> 1");
 		System.out.print(epsilon.compareTo(b)); System.out.println(" ---> -1");
+
+		RationalNumber d = new RationalNumber(3, 1); 
+		RationalNumber e = new RationalNumber(0, -13);
+		RationalNumber f = new RationalNumber(3, 0);
+		RationalNumber g = new RationalNumber(-13, 1);
+		RationalNumber h = new RationalNumber(1, -12);
+
+		System.out.println("\ntoString() test cases");
+		System.out.print(d); System.out.println(" ---> 3");
+		System.out.print(e); System.out.println(" ---> 0");
+		System.out.print(f); System.out.println(" ---> 0");
+		System.out.print(g); System.out.println(" ---> -13");
+		System.out.print(h); System.out.println(" ---> -1/12");
 	}
 }
